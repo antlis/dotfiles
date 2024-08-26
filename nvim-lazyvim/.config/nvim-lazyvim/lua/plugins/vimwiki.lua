600ab35e28368cfe5be51bee9e4cd3bd5a2d35c2
antlis <antlis850@gmail.com>
2024-08-27 03:42:58 +0500

    Add vimwiki options for lazyvim

--- nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/vimwiki.lua
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/vimwiki.lua
@@ -1,5 +1,13 @@
 return {
-  {
-    "vimwiki/vimwiki",
-  }
+  "vimwiki/vimwiki",
+  init = function()
+    vim.g.vimwiki_global_ext = 0
+    vim.g.vimwiki_list = {
+      {
+        path = '~/Documents/wiki/',
+        syntax = 'markdown',
+        ext = '.md',
+      },
+    }
+  end,
 }
