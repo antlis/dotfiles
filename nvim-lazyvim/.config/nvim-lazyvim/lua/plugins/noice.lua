aedd31a8fd168b31458c289a34f1d614adcc7aee
lad <antlis850@gmail.com>
2026-02-16 02:36:38 +0500

    set classic vim command line for lazyvim

---
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/noice.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    messages = {
+      enabled = false, -- Disable noice message handling completely
+    },
+    cmdline = {
+      enabled = false, -- Also disable cmdline for full classic experience
+    },
+  },
+}
