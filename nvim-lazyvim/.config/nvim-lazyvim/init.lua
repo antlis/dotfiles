af6a1e3c76b85e17333e68ac82e83a80141758ff
lad <antlis850@gmail.com>
2026-02-16 00:03:39 +0500

    Upate lazyvim init.lua to include zsh_private

--- nvim-lazyvim/.config/nvim-lazyvim/init.lua
+++ nvim-lazyvim/.config/nvim-lazyvim/init.lua
@@ -16,3 +16,28 @@ function CustomGF()
     vim.cmd("edit " .. vim.fn.expand("<cfile>"))
   end
 end
+
+-- Load .zsh_private from .dotfiles
+local home = os.getenv("HOME")
+local dotfiles_path = home .. "/.dotfiles/zsh/.zsh_private"
+
+local exists = vim.fn.filereadable(dotfiles_path)
+if exists == 1 then
+  local lines = vim.fn.readfile(dotfiles_path)
+
+  for _, line in ipairs(lines) do
+    if line:find("export ") then
+      local key_start = line:find("export ") + 7
+      local key_end = line:find("=", key_start) - 1
+      local key = line:sub(key_start, key_end)
+
+      local value_start = line:find('"', key_end) + 1
+      local value_end = line:find('"', value_start) - 1
+      local value = line:sub(value_start, value_end)
+
+      if key and value then
+        vim.env[key] = value
+      end
+    end
+  end
+end
