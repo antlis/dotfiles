1ace04e3c2300dc968df37872b3ac7fc371f576c
lad <antlis850@gmail.com>
2026-02-16 02:54:58 +0500

    fix lazyvim separator border colors

--- nvim-lazyvim/.config/nvim-lazyvim/lua/config/autocmds.lua
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/config/autocmds.lua
@@ -9,3 +9,22 @@ vim.api.nvim_create_user_command("FullFileCopy", function()
   vim.fn.setreg("+", path)  -- copy to clipboard (+ register)
   print("Full path copied to clipboard: " .. path)
 end, {})
+
+-- Set border colors immediately on startup
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#195466", bg = "#11151c" })
+vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#195466", bg = "#11151c" })
+vim.api.nvim_set_hl(0, "NeoTreeBorder", { fg = "#195466", bg = "#11151c" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#195466", bg = "#11151c" })
+vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { fg = "#195466", bg = "#11151c" })
+
+-- Also ensure it persists after colorscheme loads
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#195466", bg = "#11151c" })
+    vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#195466", bg = "#11151c" })
+    vim.api.nvim_set_hl(0, "NeoTreeBorder", { fg = "#195466", bg = "#11151c" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#195466", bg = "#11151c" })
+    vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { fg = "#195466", bg = "#11151c" })
+  end,
+})
