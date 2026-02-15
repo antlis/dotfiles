3892369dd2a84ebd1d92fec374fe3c4fd4629085
lad <antlis850@gmail.com>
2026-02-16 00:06:39 +0500

    setup devdocs for lazyvim

--- nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/devdocs.lua
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/devdocs.lua
@@ -1,6 +1,18 @@
 return {
   {
     "luckasRanarison/nvim-devdocs",
+    cmd = {
+      "DevdocsFetch",
+      "DevdocsInstall",
+      "DevdocsUninstall",
+      "DevdocsOpen",
+      "DevdocsOpenFloat",
+      "DevdocsOpenCurrent",
+      "DevdocsOpenCurrentFloat",
+      "DevdocsToggle",
+      "DevdocsUpdate",
+      "DevdocsUpdateAll",
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
