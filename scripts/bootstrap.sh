afc38498a497f946b9e49c0f3783cf1819fc3cab
lad <antlis@protonmail.com>
2026-03-29 23:06:15 +0500

    Update bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-
 CONFIG_DIR="$HOME/.config"
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-
 # Private repos cloned to ~/.config
 declare -A CONFIG_REPOS=(
   ["tmuxinator"]="git@github.com:antlis/tmuxinator.git"
   # add more as needed
 )
-
 # Oh-my-zsh custom plugins
 declare -A ZSH_PLUGINS=(
   ["fzf-tab"]="git@github.com:Aloxaf/fzf-tab.git"
@@ -16,7 +13,6 @@ declare -A ZSH_PLUGINS=(
   ["zsh-syntax-highlighting"]="git@github.com:zsh-users/zsh-syntax-highlighting.git"
   ["zshmarks"]="git@github.com:jocelynmallon/zshmarks.git"
 )
-
 clone_or_pull() {
   local target="$1"
   local url="$2"
@@ -28,22 +24,18 @@ clone_or_pull() {
     git clone "$url" "$target"
   fi
 }
-
 # Clone private repos into ~/.config
 for dir in "${!CONFIG_REPOS[@]}"; do
   clone_or_pull "$CONFIG_DIR/$dir" "${CONFIG_REPOS[$dir]}"
 done
-
 # Clone zsh plugins into oh-my-zsh custom plugins dir
 mkdir -p "$ZSH_CUSTOM/plugins"
 for plugin in "${!ZSH_PLUGINS[@]}"; do
   clone_or_pull "$ZSH_CUSTOM/plugins/$plugin" "${ZSH_PLUGINS[$plugin]}"
 done
-
 # Clone zsh-custom repo and symlink files to ~/.oh-my-zsh/custom/
 ZSH_CUSTOM_REPO="$HOME/.zsh-custom"
 clone_or_pull "$ZSH_CUSTOM_REPO" "git@github.com:antlis/zsh-custom.git"
-
 # Symlink aliases and functions to ZSH_CUSTOM
 for file in "$ZSH_CUSTOM_REPO"/*.zsh; do
   [ -f "$file" ] || continue
@@ -56,37 +48,32 @@ for file in "$ZSH_CUSTOM_REPO"/*.zsh; do
     echo "$filename already linked, skipping..."
   fi
 done
-
 # Clone links repo to ~/Documents/
 LINKS_REPO="$HOME/Documents/links"
 clone_or_pull "$LINKS_REPO" "git@github.com:antlis/links.git"
-
 # Symlink bookmarks to ~/.config/surfraw/bookmarks
 SURFRAW_DIR="$CONFIG_DIR/surfraw"
 if [ ! -d "$SURFRAW_DIR" ]; then
   echo "Creating $SURFRAW_DIR..."
   mkdir -p "$SURFRAW_DIR"
 fi
-
 BOOKMARKS_TARGET="$SURFRAW_DIR/bookmarks"
 BOOKMARKS_SOURCE="$LINKS_REPO/bookmarks.link"
-
 if [ ! -L "$BOOKMARKS_TARGET" ]; then
   echo "Linking bookmarks..."
   ln -sf "$BOOKMARKS_SOURCE" "$BOOKMARKS_TARGET"
 else
   echo "bookmarks already linked, skipping..."
 fi
-
 # Clone rofi-scripts to ~/bin/rofi
 BIN_DIR="$HOME/bin"
 if [ ! -d "$BIN_DIR" ]; then
   echo "Creating $BIN_DIR..."
   mkdir -p "$BIN_DIR"
 fi
-
 clone_or_pull "$BIN_DIR/rofi" "git@github.com:antlis/rofi-scripts.git"
-
+# Clone my-scripts to ~/my-scripts
+clone_or_pull "$HOME/my-scripts" "git@github.com:antlis/my-scripts.git"
 # Fetch fzf-kill binary
 LOCAL_BIN="$HOME/.local/bin"
 mkdir -p "$LOCAL_BIN"
@@ -97,5 +84,4 @@ if [ ! -f "$LOCAL_BIN/fzf-kill" ]; then
 else
   echo "fzf-kill already installed, skipping..."
 fi
-
 echo "Done!"
