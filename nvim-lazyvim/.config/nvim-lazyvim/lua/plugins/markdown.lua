d3bcdd8d0f34ff0d0a0a24d33d3e8e29bb6e2477
lad <antlis@protonmail.com>
2026-03-29 23:05:49 +0500

    Add markdown plugin for lazyvim

---
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/markdown.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+  },
+}
