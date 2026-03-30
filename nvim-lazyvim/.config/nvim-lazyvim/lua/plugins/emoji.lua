edcd81db3120fda0a143d03db5cf790b1957a263
lad <antlis@protonmail.com>
2026-03-31 00:18:27 +0500

    Set telescope fullscreen, update emoji plugin, lazyvim

--- nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/emoji.lua
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/emoji.lua
@@ -1,20 +1,47 @@
 return {
+  -- add emoji source to blink.cmp
   {
-    "allaman/emoji.nvim",
-    version = "1.0.0",
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-      "nvim-telescope/telescope.nvim",
-    },
-    opts = {
-      enable_cmp_integration = true,
-    },
-    config = function(_, opts)
-      require("emoji").setup(opts)
-      local ts = require("telescope").load_extension("emoji")
-      vim.keymap.set("n", "<leader>se", ts.emoji, { desc = "[S]earch [E]moji" })
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      opts.sources.default = vim.list_extend(opts.sources.default or {}, { "emoji" })
+      opts.sources.providers = opts.sources.providers or {}
+      opts.sources.providers.emoji = {
+        module = "blink.compat.source",
+        name = "emoji",
+        opts = { trigger = ":" },
+      }
     end,
-  }
+  },
+
+  -- keep cmp-emoji as the data source (blink.compat bridges it)
+  {
+    "hrsh7th/cmp-emoji",
+    dependencies = { "saghen/blink.compat" },
+    event = "InsertEnter",
+  },
+
+  {
+    "xiyaowong/telescope-emoji.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          emoji = {
+            action = function(emoji)
+              local mode = vim.fn.mode()
+              vim.api.nvim_put({ emoji.value }, "c", false, true)
+              if mode == "i" then
+                vim.cmd("startinsert!")
+              end
+            end,
+          },
+        },
+      })
+      require("telescope").load_extension("emoji")
+      vim.keymap.set("n", "<leader>se", function()
+        require("telescope").extensions.emoji.emoji()
+      end, { desc = "[S]earch [E]moji" })
+    end,
+  },
 }
