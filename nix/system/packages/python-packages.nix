b9f2890d0e29b7818640ed141ebe6b8b2f10586b
lad <antlis@protonmail.com>
2026-04-03 02:21:18 +0500

    Replace tmuxinator with tmuxp, creapte python packages file

---
+++ nix/system/packages/python-packages.nix
@@ -0,0 +1,5 @@
+{ pkgs, ... }:
+[
+  pkgs.uv        # Fast Python package manager | https://github.com/astral-sh/uv
+  pkgs.tmuxp     # Tmux session manager built on libtmux | https://github.com/tmux-python/tmuxp
+]
