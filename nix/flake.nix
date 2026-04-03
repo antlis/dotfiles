8842ef574c133f89f49fa2ec2e395dd8a782639b
lad <antlis@protonmail.com>
2026-04-04 03:26:56 +0500

    Add claude, and pandoc stuff

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,26 +13,56 @@
       url = "github:max-sixty/worktrunk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    claude-code = {
+      url = "github:sadjow/claude-code-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, claude-code, ... }@inputs:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config = {
+        allowUnfree = true;
+      };
+
+      overlays = [
+        inputs.claude-code.overlays.default
+      ];
+    };
+
     c = import ./constants.nix;
     privateNix = /. + c.homeDir + "/dotfiles/nix/system/private.nix";
   in
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       inherit system;
+
       specialArgs = {
         inherit inputs;
       };
+
       modules = [
         ./system/configuration.nix
         home-manager.nixosModules.home-manager
-        (builtins.path { path = /etc/nixos/hardware-configuration.nix; name = "hardware-configuration.nix"; })
-        (builtins.path { path = privateNix; name = "private.nix"; })
+
+        ({ config, ... }: {
+          nixpkgs.pkgs = pkgs;
+        })
+
+        (builtins.path {
+          path = /etc/nixos/hardware-configuration.nix;
+          name = "hardware-configuration.nix";
+        })
+
+        (builtins.path {
+          path = privateNix;
+          name = "private.nix";
+        })
       ];
     };
   };
