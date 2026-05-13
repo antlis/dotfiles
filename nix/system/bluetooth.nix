b4d0029f18ec213657ed0fe402ac1969c7f8a7dd
lad <antlis@protonmail.com>
2026-05-13 21:48:00 +0500

    enable bluetooth nix

--- nix/system/bluetooth.nix
+++ nix/system/bluetooth.nix
@@ -1,6 +1,13 @@
 { config, pkgs, lib, ... }:
 
 {
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.blueman.enable = true;
+
   environment.systemPackages = with pkgs; [
     blueman
   ];
