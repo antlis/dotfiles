3cb7165d9abef39ffa1fdc2ca52653870e4e57cd
lad <antlis@protonmail.com>
2026-08-04 13:09:06 +0500

    feat(nvim): auto-install Russian spell file

--- nvim-lazyvim/.config/nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/config/options.lua
@@ -6,3 +6,16 @@ vim.g.autoformat = false
 vim.g.autoread = true
 vim.g.ignorecase = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.spelllang = { "en", "ru" }
+
+-- Auto-install spell files if missing
+local spell_dir = vim.fn.stdpath("config") .. "/spell"
+if vim.fn.isdirectory(spell_dir) == 0 then
+  vim.fn.mkdir(spell_dir, "p")
+end
+if vim.fn.filereadable(spell_dir .. "/ru.utf-8.spl") == 0 then
+  vim.fn.system({
+    "curl", "-fLo", spell_dir .. "/ru.utf-8.spl",
+    "https://raw.githubusercontent.com/vim/vim/master/runtime/spell/russian/russian.utf-8.spl",
+  })
+end
